f1320775dfcc5f4fc15e4ba1dac16a14c4dde803
mcastre1 <miguelcastrejongal@gmail.com>
2023-12-19 19:44:06 -0700

    Change app launcher icon by creating an image asset through resource manager for the superheroes app.

---
+++ Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.superheroes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.ui.theme.SuperheroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperheroesTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun SuperheroesApp(modifier: Modifier = Modifier){
+
+}
+
+@Preview
+@Composable
+fun SuperheroesAppPreview(){
+
+}
