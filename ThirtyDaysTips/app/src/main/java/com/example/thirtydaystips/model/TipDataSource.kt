c7284c771cb887b1297e213dcc768f21b7afeacd
mcastre1 <miguelcastrejongal@gmail.com>
2023-12-24 14:25:35 -0700

    Added all string values for each of the days.

---
+++ ThirtyDaysTips/app/src/main/java/com/example/thirtydaystips/model/TipDataSource.kt
@@ -0,0 +1,13 @@
+package com.example.thirtydaystips.model
+
+import com.example.thirtydaystips.R
+
+object TipDataSource {
+    val tips = listOf(
+        Tip(
+            titleRes = R.string.app_name,
+            imageRes = R.drawable.ic_launcher_background,
+            descRes = R.string.app_name
+        )
+    )
+}
