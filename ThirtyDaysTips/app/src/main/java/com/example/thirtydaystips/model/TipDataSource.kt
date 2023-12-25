d9b1c610c310a925122f7a1f8cde90894ed28b2c
mcastre1 <miguelcastrejongal@gmail.com>
2023-12-24 21:57:42 -0700

    Added the rest of tips into the tips list in tipdatasource.

--- ThirtyDaysTips/app/src/main/java/com/example/thirtydaystips/model/TipDataSource.kt
+++ ThirtyDaysTips/app/src/main/java/com/example/thirtydaystips/model/TipDataSource.kt
@@ -9,6 +9,180 @@ object TipDataSource {
             titleRes = R.string.tip1,
             imageRes = R.drawable.day1,
             descRes = R.string.desc1
+        ),
+        Tip(
+            dayRes = R.string.day2,
+            titleRes = R.string.tip2,
+            imageRes = R.drawable.day2,
+            descRes = R.string.desc2
+        ),
+        Tip(
+            dayRes = R.string.day3,
+            titleRes = R.string.tip3,
+            imageRes = R.drawable.day3,
+            descRes = R.string.desc3
+        ),
+        Tip(
+            dayRes = R.string.day4,
+            titleRes = R.string.tip4,
+            imageRes = R.drawable.day4,
+            descRes = R.string.desc4
+        ),
+        Tip(
+            dayRes = R.string.day5,
+            titleRes = R.string.tip5,
+            imageRes = R.drawable.day5,
+            descRes = R.string.desc5
+        ),
+        Tip(
+            dayRes = R.string.day6,
+            titleRes = R.string.tip6,
+            imageRes = R.drawable.day6,
+            descRes = R.string.desc6
+        ),
+        Tip(
+            dayRes = R.string.day7,
+            titleRes = R.string.tip7,
+            imageRes = R.drawable.day7,
+            descRes = R.string.desc7
+        ),
+        Tip(
+            dayRes = R.string.day8,
+            titleRes = R.string.tip8,
+            imageRes = R.drawable.day8,
+            descRes = R.string.desc8
+        ),
+        Tip(
+            dayRes = R.string.day9,
+            titleRes = R.string.tip9,
+            imageRes = R.drawable.day9,
+            descRes = R.string.desc9
+        ),
+        Tip(
+            dayRes = R.string.day10,
+            titleRes = R.string.tip10,
+            imageRes = R.drawable.day10,
+            descRes = R.string.desc10
+        ),
+        Tip(
+            dayRes = R.string.day11,
+            titleRes = R.string.tip11,
+            imageRes = R.drawable.day11,
+            descRes = R.string.desc11
+        ),
+        Tip(
+            dayRes = R.string.day12,
+            titleRes = R.string.tip12,
+            imageRes = R.drawable.day12,
+            descRes = R.string.desc12
+        ),
+        Tip(
+            dayRes = R.string.day13,
+            titleRes = R.string.tip13,
+            imageRes = R.drawable.day13,
+            descRes = R.string.desc13
+        ),
+        Tip(
+            dayRes = R.string.day14,
+            titleRes = R.string.tip14,
+            imageRes = R.drawable.day14,
+            descRes = R.string.desc14
+        ),
+        Tip(
+            dayRes = R.string.day15,
+            titleRes = R.string.tip15,
+            imageRes = R.drawable.day15,
+            descRes = R.string.desc15
+        ),
+        Tip(
+            dayRes = R.string.day16,
+            titleRes = R.string.tip16,
+            imageRes = R.drawable.day16,
+            descRes = R.string.desc16
+        ),
+        Tip(
+            dayRes = R.string.day17,
+            titleRes = R.string.tip17,
+            imageRes = R.drawable.day17,
+            descRes = R.string.desc17
+        ),
+        Tip(
+            dayRes = R.string.day18,
+            titleRes = R.string.tip18,
+            imageRes = R.drawable.day18,
+            descRes = R.string.desc18
+        ),
+        Tip(
+            dayRes = R.string.day19,
+            titleRes = R.string.tip19,
+            imageRes = R.drawable.day19,
+            descRes = R.string.desc19
+        ),
+        Tip(
+            dayRes = R.string.day20,
+            titleRes = R.string.tip20,
+            imageRes = R.drawable.day20,
+            descRes = R.string.desc20
+        ),
+        Tip(
+            dayRes = R.string.day21,
+            titleRes = R.string.tip21,
+            imageRes = R.drawable.day21,
+            descRes = R.string.desc21
+        ),
+        Tip(
+            dayRes = R.string.day22,
+            titleRes = R.string.tip22,
+            imageRes = R.drawable.day22,
+            descRes = R.string.desc22
+        ),
+        Tip(
+            dayRes = R.string.day23,
+            titleRes = R.string.tip23,
+            imageRes = R.drawable.day23,
+            descRes = R.string.desc23
+        ),
+        Tip(
+            dayRes = R.string.day24,
+            titleRes = R.string.tip24,
+            imageRes = R.drawable.day24,
+            descRes = R.string.desc24
+        ),
+        Tip(
+            dayRes = R.string.day25,
+            titleRes = R.string.tip25,
+            imageRes = R.drawable.day25,
+            descRes = R.string.desc25
+        ),
+        Tip(
+            dayRes = R.string.day26,
+            titleRes = R.string.tip26,
+            imageRes = R.drawable.day26,
+            descRes = R.string.desc26
+        ),
+        Tip(
+            dayRes = R.string.day27,
+            titleRes = R.string.tip27,
+            imageRes = R.drawable.day27,
+            descRes = R.string.desc27
+        ),
+        Tip(
+            dayRes = R.string.day28,
+            titleRes = R.string.tip28,
+            imageRes = R.drawable.day28,
+            descRes = R.string.desc28
+        ),
+        Tip(
+            dayRes = R.string.day29,
+            titleRes = R.string.tip29,
+            imageRes = R.drawable.day29,
+            descRes = R.string.desc29
+        ),
+        Tip(
+            dayRes = R.string.day30,
+            titleRes = R.string.tip30,
+            imageRes = R.drawable.day30,
+            descRes = R.string.desc30
         )
     )
 }
