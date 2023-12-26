394494f716a1b5c761aef40886fd5b3e9f3e02be
mcastre1 <miguelcastrejongal@gmail.com>
2023-12-26 07:05:43 -0700

    Made cards clickable and expandable/retractable

--- ThirtyDaysTips/app/src/main/java/com/example/thirtydaystips/MainActivity.kt
+++ ThirtyDaysTips/app/src/main/java/com/example/thirtydaystips/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.thirtydaystips.model.TipDataSource
 import com.example.thirtydaystips.ui.theme.ThirtyDaysTipsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ThirtyDaysApp() {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ThirtyDaysTipsTheme {
-        Greeting("Android")
+        ) {
+            val tips = TipDataSource.tips
+            TipList(tips = tips, contentPadding = it)
+        }
     }
 }
--- ThirtyDaysTips/app/src/main/java/com/example/thirtydaystips/TipsScreen.kt
+++ ThirtyDaysTips/app/src/main/java/com/example/thirtydaystips/TipsScreen.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -42,11 +46,13 @@ class TipsScreen {
 
     @Composable
     fun TipListItem(tip: Tip, modifier: Modifier = Modifier) {
+        var expanded by remember { mutableStateOf(false) }
+
         Card(
             elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
             modifier = modifier
                 .fillMaxWidth()
-                .clickable { }
+                .clickable {expanded = !expanded}
         ) {
             Column(modifier = Modifier.padding(16.dp)) {
                 Text(text = stringResource(id = tip.dayRes))
@@ -59,7 +65,9 @@ class TipsScreen {
                         .fillMaxWidth()
                         .height(194.dp)
                 )
-                Text(text = stringResource(id = tip.descRes))
+                if(expanded){
+                    Text(text = stringResource(id = tip.descRes))
+                }
             }
         }
     }
